ea592875c4876edbb6ef8092187654d2a094db47
unknown <matthiaskrh123@gmail.com>
2022-10-22 18:17:16 -0700

    Light control

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,8 +10,19 @@ public class PlayerController : MonoBehaviour
 
     public float runSpeed = 20.0f;
     Camera maincam;
+
+    public GameObject flashlight;
+    LightScript lightScript;
+    public float maxAngle;
+    public float minAngle;
+    public float angleChangeSpeed;
+
+    bool isLightOff = false;
+
+
     void Start()
     {
+        lightScript = flashlight.GetComponent<LightScript>();
         maincam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         body = GetComponent<Rigidbody2D>(); 
     }
@@ -19,11 +30,34 @@ public class PlayerController : MonoBehaviour
     void Update ()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical"); 
+        vertical = Input.GetAxisRaw("Vertical");
+
+        // Light beam adjustment
+        if(Input.GetKey(KeyCode.Q)){
+            lightScript.angle += angleChangeSpeed * Time.deltaTime;
+        } 
+
+        if(Input.GetKey(KeyCode.E)){
+            lightScript.angle -= angleChangeSpeed * Time.deltaTime;
+        }
+
+        lightScript.angle  = Mathf.Clamp(lightScript.angle, minAngle, maxAngle);
+
+
+        // Light toggling
+        if(Input.GetKeyDown(KeyCode.F)){
+            if(isLightOff){
+                flashlight.SetActive(true);
+            } else {
+                flashlight.SetActive(false);
+            }
+            isLightOff = !isLightOff;
+        }
     }
 
     private void FixedUpdate()
     {  
+
         // Movement
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
 
